7d1d1ca917a9e2b12b13d96f947442b031120902
Andrew Smith <andrew.smith@cuc.org>
2019-02-21 10:57:01 -0600

    added selenium test

---
+++ AzureBuildAndDeploy.Tests/SeleniumTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace AzureBuildAndDeploy.Tests
+{
+    [TestFixture]
+    public class SeleniumTests
+    {
+        [Test]
+        public void SeleniumTest()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.Close();
+            Assert.Pass();
+        }
+
+    }
+}
